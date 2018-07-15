ab3fde387427a47e06cffb6764afd83372ad519b
huazhenxia <imxhz@foxmail.com>
2018-07-15 00:06:02 -0700

    menu component

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
 
+import { Menu, Icon } from 'antd';
+
+import MenuConfig from '../../config/menuConfig';
+import './index.less';
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
 export default class NavLeft extends Component{
     render(){
         return (
-            <div>NavLeft</div>
+            <div>
+                <div className="logo">
+                    <h1>Management</h1>
+                </div>
+                <Menu theme="dark">
+                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                        <Menu.Item key="1">Option 1</Menu.Item>
+                        <Menu.Item key="2">Option 2</Menu.Item>
+                        <Menu.Item key="3">Option 3</Menu.Item>
+                        <Menu.Item key="4">Option 4</Menu.Item>
+                    </SubMenu>
+                </Menu>
+            </div>
         )
     }
 }
