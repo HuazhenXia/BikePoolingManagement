22edbcc7983b457e15056f34f6e79badb89bbd4d
huazhenxia <imxhz@foxmail.com>
2018-07-14 22:35:19 -0700

    basic layout

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -192,7 +192,13 @@ module.exports = {
                 },
               },
               {
-                loader: require.resolve('less-loader')
+                loader: require.resolve('less-loader'),
+                options: {
+                  modules: false,
+                  modifyVars: {
+                    "@primary-color": "#f9c700"
+                  }
+                }
               }
             ],
           },
