d17c0181ddc69d0994149f3def22409732f1bde0
huazhenxia <imxhz@foxmail.com>
2018-07-15 23:06:08 -0700

    weather

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import {Row, Col} from 'antd';
 
 import './index.less'
 import Util from '../../utils/utils';
+import axios from '../../axios/index';
 
 
 export default class Header extends Component{
@@ -18,8 +19,21 @@ export default class Header extends Component{
                 sysTime
             })
         }, 1000)
+        this.getWeatherAPIData()
     }
 
+    getWeatherAPIData(){
+        let city = '北京';
+        axios.jsonp({
+            url:'http://api.map.baidu.com/telematics/v3/weather?location='+ encodeURIComponent(city) +'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2',
+        }).then(res => {
+            let data = res.results[0].weather_data[0];
+            this.setState({
+                dayPictureUrl: data.dayPictureUrl,
+                weather: data.weather
+            })
+        }) 
+    }
 
     render(){
         return (
@@ -34,7 +48,10 @@ export default class Header extends Component{
                     </Col>
                     <Col span="20" className="weather">
                         <span className="date">{this.state.sysTime}</span>
-                        <span className="weather-detail"> Sunny</span>
+                        <span className="weather-img">
+                            <img src={this.state.dayPictureUrl} alt=""/>
+                        </span>
+                        <span className="weather-detail">{this.state.weather}</span>
                     </Col>
                 </Row>
             </div>
