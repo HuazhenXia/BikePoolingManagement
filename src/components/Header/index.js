ddef862a8feca34520415636493b66fda73e78c1
huazhenxia <imxhz@foxmail.com>
2018-07-15 20:32:04 -0700

    header and time

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,43 @@
 import React, {Component} from 'react';
 
+import {Row, Col} from 'antd';
+
+import './index.less'
+import Util from '../../utils/utils';
+
+
 export default class Header extends Component{
+    componentWillMount(){
+        this.setState({
+            username: 'Aaron'
+        })
+
+        setInterval(() => {
+            let sysTime = Util.formateDate(new Date().getTime());
+            this.setState({
+                sysTime
+            })
+        }, 1000)
+    }
+
+
     render(){
         return (
-            <div>This is Header</div>
+            <div className="header">
+                <Row className="header-top">
+                    <span>Welcome, {this.state.username}! </span>
+                    <a href="">LOG OUT</a>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span="4" className="bread-crumb-title">
+                        Home
+                    </Col>
+                    <Col span="20" className="weather">
+                        <span className="date">{this.state.sysTime}</span>
+                        <span className="weather-detail"> Sunny</span>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
