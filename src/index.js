518418e4110f558c110e6159ed121397e4ed181b
huazhenxia <imxhz@foxmail.com>
2018-07-23 23:37:29 -0700

    react-router-dom demo mix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Admin from './Admin';
+// import Admin from './Admin';
+import Home from './pages/route_demo/Home';
 
-ReactDOM.render(<Admin />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/pages/route_demo/Topics.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Topics extends Component{
+    render(){
+        return (
+           <div>
+               This is Topics page
+           </div>
+        );
+    }
+}
+
+export default Topics;
